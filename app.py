1b329ae07447b69583fda45c45bc17622d4343eb
tikachy <tikaramchaudhary03gmail.com>
2023-03-25 14:05:22 +0545

    inital commit

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('hlo world')
+secert_number=9
+guess_count=0
+guess_limit=3
+while guess_count<guess_limit:
+    guess= int(input('Guess: '))
+    guess_count +=1
+    if guess==secert_number:
+        print("u win")
+        break
+else:
+    print("sorry u failed")
